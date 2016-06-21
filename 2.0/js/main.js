01c6fd6d7a673a37e4156746216e92f8bc575e17
nataleedesotell <nataleedesotell@gmail.com>
2016-06-20 21:52:13 -0500

    added datatables plugin and example data
    a sample table is showing up with interactivity on the home page -- need
    to load in actual data and find a more appropriate place to put it

---
+++ 2.0/js/main.js
@@ -0,0 +1,9 @@
+//wait for the page to load
+//select the table we want
+//run the DataTables function on it
+
+$(document).ready(function() {
+    $('#example').DataTable();
+} );
+
+//DataTables adds ordering, searching, paging, and information to the table by default
